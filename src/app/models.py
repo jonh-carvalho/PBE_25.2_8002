20c81efa4b38bb60f350e146096c3f9c6fc4af50
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2025-08-15 15:49:42 -0300

    src

---
+++ src/app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
