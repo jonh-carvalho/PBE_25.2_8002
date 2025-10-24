7a654be14436ea6c1c76fe9443bd05e7cc52d90e
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2025-10-24 17:42:39 -0300

    Update

--- src/Streaming/streaming_platform/settings.py
+++ src/Streaming/streaming_platform/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'content_app',
 ]
 
@@ -47,7 +48,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'streaming_platform.urls'
 
